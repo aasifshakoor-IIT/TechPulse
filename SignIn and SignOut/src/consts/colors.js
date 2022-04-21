a1d926f57847c298a90cfd4b5ba2cae410898026
aasifshakoor <95273523+aasifshakoor@users.noreply.github.com>
2022-04-22 00:52:59 +0530

    Updating Colour.Js
    Adding Extra colours / for export default COLORS

--- SignIn and SignOut/src/consts/colors.js
+++ SignIn and SignOut/src/consts/colors.js
@@ -8,6 +8,17 @@ const COLORS = {
   tranparent: 'rgba(0,0,0,0)',
   brown: '#B2AB8C',
   darkblue: '#47597E',
+  darkBlue: "#111A2C",
+  darkGray: "#525C67",
+  darkGray2: "#757D85",
+  gray: "#898B9A",
+  gray2: "#BBBDC1",
+  gray3: '#CFD0D7',
+  lightGray1: "#DDDDDD",
+  lightGray2: "#F5F5F8",
+  white2: "#FBFBFB",
+  white: '#FFFFFF',
+  black: "#000000",
 };
 
 export default COLORS;
