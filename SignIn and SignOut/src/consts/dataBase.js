ce1ef9aa3ad4a7765821e18cbef7d7622ab4eea8
aasifshakoor <95273523+aasifshakoor@users.noreply.github.com>
2022-04-22 02:28:00 +0530

    Updating Database.js
    Database was running on  localhost / "mongodb://192.168.1.2:27017/mydb changed to imported database .
    
    -- Code --
    
    //const mongoose = require('mongoose');
    
    //var mongo = require('mongodb');
    
    //var url = "mongodb://localhost:27017/mydb";
    
    var MongoClient = require('mongodb').MongoClient;
    var url = "mongodb://192.168.1.2:27017/mydb";
    
    MongoClient.connect(url, function(err, db) {
        if (err) throw err;
        var dbo = db.db("mydb");
        dbo.createCollection("customers", function(err, res) {
          if (err) throw err;
          console.log("Collection created!");
          db.close();
        });
      });

--- SignIn and SignOut/src/consts/dataBase.js
+++ SignIn and SignOut/src/consts/dataBase.js
@@ -1,18 +1,44 @@
-//const mongoose = require('mongoose');
+const houses = [
+  {
+    id: '1',
+    title: 'Keels',
+    location: 'Colombo 03',
+    image: require('../assets/house1.jpg'),
+    isFavourite: true,
+    details: `Keels is a Sri Lankan shopping mall which is located at Western Province, Kollupitiya, Colombo.  `,
+   interiors: [
+      
+    ],
+  },
+   {
+    id: '2',
+    title: 'ODEL',
+    location: 'Colombo 10',
+    isFavourite: false,
+    image: require('../assets/house2.jpg'),
+    details: `This building is located in the Oliver area, withing walking distance of shops...`,
+  
+  },
+   {
+    id: '3',
+    title: 'Dialog Axiata ',
+    location: 'Nawala',
+    isFavourite: false,
+    image: require('../assets/house3.jpg'),
+    details: `This building is located in the Oliver area, withing walking distance of shops...`,
+  
+  },
+   {
+    id: '4',
+    title: 'Hemas Hospitals',
+    location: 'Colombo',
+    isFavourite: false,
+    image: require('../assets/house4.jpg'),
+    details: `This building is located in the Oliver area, withing walking distance of shops...`,
+    
+  },
+];
 
-//var mongo = require('mongodb');
 
-//var url = "mongodb://localhost:27017/mydb";
+export default houses;
 
-var MongoClient = require('mongodb').MongoClient;
-var url = "mongodb://192.168.1.2:27017/mydb";
-
-MongoClient.connect(url, function(err, db) {
-    if (err) throw err;
-    var dbo = db.db("mydb");
-    dbo.createCollection("customers", function(err, res) {
-      if (err) throw err;
-      console.log("Collection created!");
-      db.close();
-    });
-  }); 
