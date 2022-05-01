1ff79b5a1e85028b1d1327a638597c8f0b3e21a7
Kausika19 <85722759+Kausika19@users.noreply.github.com>
2022-05-01 16:31:01 +0530

    Update AboutUs.js

--- src/screens/AboutUs/AboutUs.js
+++ src/screens/AboutUs/AboutUs.js
@@ -3,10 +3,13 @@ import{
     View,
     StyleSheet,
     Text,
-    Image
+    Image,
+    Pressable
 }from 'react-native';
+import CustomButton from '../../components/CustomButton';
 import COLORS from '../../consts/colors';
 import Logo from '../../../assets/images/Parkit_Logo.png';
+import { Linking } from '@aws-amplify/core';
 
 /*export default function Map(){*/
 
@@ -30,15 +33,82 @@ const AboutUs = () => {
            FRONT-END DEVELOPER DETAILS
          </Text>
          <Text style={styles.text3}>
-           Email: frontend@gmail.com{"\n"} 
-           Contact Number: 01234
-         </Text>
+           Email: 
+
+         <Pressable
+      onPress={() => Linking.openURL('mailto:aasif.2019089@iit.ac.lk')}
+      style={[
+        styles.container,
+      ]}>
+      <Text
+        style={[
+          styles.text,
+        ]}>
+        {" aasif.2019089@iit.ac.lk"}
+      </Text>
+        </Pressable>
+
+        <Pressable
+      onPress={() => Linking.openURL('mailto:kaushika.20200657@iit.ac.lk')}
+      style={[
+        styles.container,
+      ]}>
+      <Text
+        style={[
+          styles.text,
+        ]}>
+        {"            kaushika.20200657@iit.ac.lk"}
+      </Text>
+        </Pressable>
+      </Text>
+
+
+
          <Text style={{fontSize: 18, marginRight: 50, padding: 15,color: COLORS.darkblue, fontWeight: "bold"}}> 
            BACK-END DEVELOPER DETAILS 
          </Text>
          <Text style={styles.text3}>
-           Email: backend@gmail.com{"\n"} 
-           Contact Number: 56789
+           Email: 
+           
+           <Pressable
+      onPress={() => Linking.openURL('mailto:vinuri.2019753@iit.ac.lk')}
+      style={[
+        styles.container,
+      ]}>
+      <Text
+        style={[
+          styles.text,
+        ]}>
+        {" vinuri.2019753@iit.ac.lk"}
+      </Text>
+        </Pressable>
+
+        <Pressable
+      onPress={() => Linking.openURL('mailto:fathima.20200253@iit.ac.lk')}
+      style={[
+        styles.container,
+      ]}>
+      <Text
+        style={[
+          styles.text,
+        ]}>
+        {"            fathima.20200253@iit.ac.lk"}
+      </Text>
+        </Pressable>
+
+        <Pressable
+      onPress={() => Linking.openURL('mailto:tanuk.20200815@iit.ac.lk')}
+      style={[
+        styles.container,
+      ]}>
+      <Text
+        style={[
+          styles.text,
+        ]}>
+        {"            tanuk.20200815@iit.ac.lk"}
+      </Text>
+        </Pressable>
+
          </Text>
 
          
@@ -58,7 +128,23 @@ const styles = StyleSheet.create({
         maxWidth: 300,
         maxHeight: 180,
       },
-    text: {
+      container: {
+        width: '100%',
+    
+        padding: 1,
+        marginVertical: 5,
+    
+        alignItems: 'center',
+        borderRadius: 5,
+      },
+        
+      text: {
+        fontWeight: 'bold',
+        color: 'black',
+        fontSize: 17 
+      },
+
+    text1: {
         fontSize: 40,
         marginTop: 20,
         color: COLORS.blue,
