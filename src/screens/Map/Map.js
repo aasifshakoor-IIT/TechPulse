787e1c50dc1b22ca6d7451ce2a03db4c0ba724f6
aasifshakoor <95273523+aasifshakoor@users.noreply.github.com>
2022-05-04 11:22:45 +0530

    Update Map.js

--- src/screens/Map/Map.js
+++ src/screens/Map/Map.js
@@ -6,6 +6,15 @@ export default class App extends Component {
   _goToYosemite() {
     openMap({ latitude: 6.8496, longitude: 79.8775 });
   }
+  render() {
+    return (
+      <Button
+        color={'#bdc3c7'}
+        onPress={this._goToYosemite}
+        title="Click To Open Maps 🗺" />
+    );
+  }
+}
 
 
 
